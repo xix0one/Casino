79e3a39c06c9f553c93548d08c7a1e9e9b4b81fc
xix0one <petrovalex2014@ya.ru>
2024-02-01 09:41:31 +0800

    Добавьте файлы проекта.

---
+++ Casino/Player.cs
@@ -0,0 +1,24 @@
+﻿namespace Casino
+{
+    class Player
+    {
+        public string Name;
+        public int Cash;
+
+        public void WriteMyInfo()
+        {
+            Console.WriteLine(Name + " has " + Cash + " bucks!");
+        }
+
+        public int GiveCash(int amount)
+        {
+            Cash -= amount;
+            return amount;
+        }
+
+        public void ReceiveCash(int amount)
+        {
+            Cash += amount;
+        }
+    }
+}
---
+++ Casino/Program.cs
@@ -0,0 +1,38 @@
+﻿using Casino;
+
+Random random = new Random();
+double odds = 0.75;
+Player player = new Player() { Name = "The player", Cash = 100 };
+
+Console.WriteLine($"Welcome to the casino. The odds are {odds}");
+
+
+while (player.Cash > 0)
+{
+    player.WriteMyInfo();
+
+    Console.Write("How much do you want to bet: ");
+    string howMuch = Console.ReadLine();
+
+    if (int.TryParse(howMuch, out int amount))
+    {
+        if (amount > player.Cash)
+            Console.WriteLine("nope");
+        else
+        { 
+            int pot = amount * 2;
+            if (random.Next(0, 2) > odds)
+            {
+                player.ReceiveCash(pot);
+                Console.WriteLine("You win " + pot);
+            }
+            else
+            {
+                player.GiveCash(amount);
+                Console.WriteLine("Bad luck, you lose");
+            }
+        }
+    }
+    else Console.WriteLine("nope!");
+}
+Console.WriteLine("The house always win.");
